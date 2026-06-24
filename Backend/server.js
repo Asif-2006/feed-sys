7f4152c027591351dafbdd662b90cf75e6445a37
Asif <asif21012006@gmail.com>
2026-06-24 14:04:48 +0530

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,8 @@ connectDB();
 
 
 
-app.listen(3000,(req,res)=>{
-    console.log("Server is running")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
